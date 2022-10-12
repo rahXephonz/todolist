e026820eef7f22f13535047b58f2c4f1294d02cf
ioofy <mynev.id@gmail.com>
2022-10-12 13:46:22 +0700

    feat: adding comments for parse utility

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,4 +1,6 @@
+// parse int eg 1 into true
 const toBool = <T>(val: T) => Boolean(val).valueOf();
+// parse bool eg boolean into 1
 const toNumber = <T>(val: T) => Number(val).valueOf();
 
 export default { toBool, toNumber };
